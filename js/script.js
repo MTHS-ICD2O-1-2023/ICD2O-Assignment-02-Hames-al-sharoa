75f64bdad9031c1c7c30e48298895a09f6711a31
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-04-15 02:35:23 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames.al-sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames.al-sharoa
+// Created on: apr 2024
 // This file contains the JS functions for index.html
 
-function calculateArea() {
-    const length = parseFloat(document.getElementById('length').value);
-    const width = parseFloat(document.getElementById('width').value);
-    
-    const area = length * width;
-    
-    document.getElementById('result').innerHTML = "The area of the rectangle is: " + area;
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateRectangleArea() {
+  // Get the values of length and width from input fields
+  const length = parseFloat(document.getElementById('length').value);
+  const width = parseFloat(document.getElementById('width').value);
+  // Check if the inputs are valid numbers
+  if (isNaN(length) || isNaN(width)) {
+    document.getElementById('area').innerHTML = 'Please enter valid numbers for length and width.';
+    return;
+  }
+  // Calculate the area of the rectangle
+  const area = length * width;
+  // Display the result
+  document.getElementById('area').innerHTML = `Area of the rectangle: ${area} square units.`;
 }
