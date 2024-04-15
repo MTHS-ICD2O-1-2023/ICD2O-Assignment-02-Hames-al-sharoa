5efadee77929957534a2497a0071e99cccdc5e42
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-04-15 02:24:23 +0000

    save

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function calculateArea() {
+    const length = parseFloat(document.getElementById('length').value);
+    const width = parseFloat(document.getElementById('width').value);
+    
+    const area = length * width;
+    
+    document.getElementById('result').innerHTML = "The area of the rectangle is: " + area;
+}
