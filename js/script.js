0a045670f5dc22390b8d02e22e8f6c0b20b3feba
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-06-06 17:57:54 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-// Copyright (c) 2024 hames.al-sharoa All rights reserved
+// Copyright (c) 2024 hames al sharoa All rights reserved
 //
-// Created by: hames.al-sharoa
-// Created on: apr 2024
+// Created by: hames alsharoa
+// Created on: feb 2024
 // This file contains the JS functions for index.html
 
 "use strict"
-
 /**
- * This function calculates area of a triangle.
+ * This function calculates the perimeter of a square.
  */
-function calculateRectangleArea() {
-  // Get the values of length and width from input fields
-  const length = parseFloat(document.getElementById('length').value);
-  const width = parseFloat(document.getElementById('width').value);
-  // Check if the inputs are valid numbers
-  if (isNaN(length) || isNaN(width)) {
-    document.getElementById('area').innerHTML = 'Please enter valid numbers for length and width.';
-    return;
-  }
-  // Calculate the area of the rectangle
-  const area = length * width;
-  // Display the result
-  document.getElementById('area').innerHTML = `Area of the rectangle: ${area} square units.`;
+function calculateClicked() {
+  // input
+  const side = parseInt(document.getElementById("side-length").value)
+
+  // process
+  const perimeter = side * 4
+
+  // output
+  document.getElementById("perimeter").innerHTML =
+    "<p>Perimeter = " + perimeter + " cm."
 }
